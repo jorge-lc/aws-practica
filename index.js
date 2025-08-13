46e520e50d5b898db2dac291f097d68030f1c07a
jorge-lc <54570298+jorge-lc@users.noreply.github.com>
2025-08-13 20:03:04 -0300

    update

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola AWS desde Node.js\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (req.url === '/') {
+        res.end('Hola AWS desde Node.js\n');
+    } else if (req.url === '/saludo') {
+        res.end('Este es el segundo endpoint\n');
+    } else if (req.url === '/saludito') {
+        res.end('Este es el tercer endpoint\n');
+    } else {
+        res.statusCode === 404;
+        res.end('Endpoint no encontrado\n');
+    }
 });
 
 server.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
-});
+    console.log(`Servidor corriendo en puerto ${port}`)
+})
